cb2b423a2c0a4d275a9ba6a52eabdc81b5bd081d
Shyam Arjarapu <shyam.arjarapu@gmail.com>
2017-08-21 07:31:32 -0500

    added api scripts files and original sample config

--- roles/opsmanager/templates/api.tutorials.sh
+++ roles/opsmanager/templates/api.tutorials.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+owner='shyam.arjarapu@10gen.com'
+ownerApiKey='0c72df21-3154-46d6-9db2-2de40d496ce7'
+url='ec2-54-214-214-22.us-west-2.compute.amazonaws.com:8080'
+group_id='5998d2fcbba0ea27746e067d'
+
+username='shyam.arjarapu@10gen.com'
+apiKey='0c72df21-3154-46d6-9db2-2de40d496ce7'
+
 # get groups 
-curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" "http://ec2-54-214-214-22.us-west-2.compute.amazonaws.com:8080/api/public/v1.0/groups" --digest -X GET
+curl -u "$owner:$ownerApiKey" "http://$url/api/public/v1.0/groups" --digest -X GET
 
 # create new group
-curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" -H "Content-Type: application/json" "http://ec2-54-214-214-22.us-west-2.compute.amazonaws.com:8080/api/public/v1.0/groups" --digest -i -X POST --data '
+curl -u "$owner:$ownerApiKey" -H "Content-Type: application/json" "http://$url/api/public/v1.0/groups" --digest -i -X POST --data '
 {
    "name": "opsmgs_auto"
 }'
@@ -14,7 +22,7 @@ curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" -H "Cont
 # 747862e5601e5eda9ff73077cb69d331
 
 # create a new user as group owner for above group
-curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" -H "Content-Type: application/json" "http://ec2-54-214-214-22.us-west-2.compute.amazonaws.com:8080/api/public/v1.0/users" --digest -i -X POST --data '
+curl -u "$owner:$ownerApiKey" -H "Content-Type: application/json" "http://$url/api/public/v1.0/users" --digest -i -X POST --data '
 {
    "username": "shyam.arjarapu@gmail.com",
    "emailAddress": "shyam.arjarapu@gmail.com",
@@ -22,14 +30,23 @@ curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" -H "Cont
    "lastName": "Arjarapu",
    "password": "p@$$w0rd",
    "roles": [{
-     "groupId": "5998d2fcbba0ea27746e067d",
+     "group_id": "$group_id",
      "roleName": "GROUP_OWNER"
    }]
 }'
 
 # Retrieve the automation configuration from Ops Manager
-curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" "http://ec2-54-214-214-22.us-west-2.compute.amazonaws.com:8080/api/public/v1.0/groups/5998d2fcbba0ea27746e067d/automationConfig" --digest -i
+curl -u "$username:$apiKey" "http://$url/api/public/v1.0/groups/$group_id/automationConfig" --digest -i
 
 # Send the updated automation configuration.
-curl -u "shyam.arjarapu@10gen.com:0c72df21-3154-46d6-9db2-2de40d496ce7" -H "Content-Type: application/json" "http://ec2-54-214-214-22.us-west-2.compute.amazonaws.com:8080/api/public/v1.0/groups/5998d2fcbba0ea27746e067d/automationConfig" --digest -i -X PUT --data @/Users/shyamarjarapu/Code/work/git-hub/sarjarapu/ansible/roles/opsmanager/templates/automation.config.json
+curl -u "$username:$apiKey" -H "Content-Type: application/json" "http://$url/api/public/v1.0/groups/$group_id/automationConfig" --digest -i -X PUT --data @/Users/shyamarjarapu/Code/work/git-hub/sarjarapu/ansible/roles/opsmanager/templates/automation.config.json
 
+# Confirm successful update of the automation configuration.
+curl -u "$username:$apiKey" "http://$url/api/public/v1.0/groups/$group_id/automationConfig" --digest -i
+
+
+# Verify that the configuration update is deployed.
+curl -u "$username:$apiKey" "http://$url/api/public/v1.0/groups/$group_id/automationStatus" --digest -i
+
+
+curl -u "$username:$apiKey" -H "Content-Type: application/json" "http://$url/api/public/v1.0/groups/$group_id/automationConfig" --digest -i -X PUT --data @/Users/shyamarjarapu/Code/work/git-hub/sarjarapu/ansible/roles/opsmanager/templates/automation.config.json
